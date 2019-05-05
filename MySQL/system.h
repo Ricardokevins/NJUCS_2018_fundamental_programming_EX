1d13380c048a106aeed8ecef1e0fb3567840c259
kevinpro <43642508+Ricardokevins@users.noreply.github.com>
2019-05-05 22:40:02 +0800

    Update system.h

--- MySQL/system.h
+++ MySQL/system.h
@@ -1,6 +1,7 @@
 #pragma once
 #include"alldefine.h"
 #include"user.h"
+
 class mysystem {
 public:
 	friend class user;
@@ -8,7 +9,12 @@ public:
 	vector<user>cur_user;
 	vector<string>table_name;//保存当前存在的数据库名字
 	vector<string>file_name;//保存当前存在的文件的名字
+	vector<string> command_spilted;
+	int length_command;
 	vector<mydata>all_mydata;
+
+	vector<string> command_split(const string &s, const string &seperator);
+
 	int cur_user_ID;
 
 	bool login_ornot;
@@ -25,6 +31,17 @@ public:
 
 	int login_user();
 
+	int read_power_file();
+
+	int update_powerfile(int i);
+
+	int exam_power(string A,int file_pos);
+
+	int revoke_power(vector<vector<int>> &A, int grantor, int granted,int file_pos,int power_pos);
+
+	int send_power_req(int power_ID,int file_pos,int user_pos);
+	int exam_power_req();
+	int multi_file();
 
 	int create();
 	int drop();
@@ -35,9 +52,8 @@ public:
 	int grant();
 	int revoke();
 	int command_analyse();
+	int read_table_file();
 	int file_in(string path, mydata &A);
-	int  file_out(mydata A);
-	vector<string> command_split(const string &s, const string &seperator);
-	vector<string> command_spilted;
-	int length_command;
+	int  file_out(string path,mydata &A);
+
 };
