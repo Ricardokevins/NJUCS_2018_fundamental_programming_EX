a9ce4919ebb19f81ab5b637d3521f53370c2bc40
kevinpro <43642508+Ricardokevins@users.noreply.github.com>
2019-06-07 10:15:12 +0800

    Add files via upload

---
+++ prolog_interpreter/test3.pl
@@ -0,0 +1,12 @@
+playsAirGuitar(yolanda , kevin):- listens3music(yolanda,mia), father(yolanda,mia).
+room(cellar). 
+door(kitchen, cellar).
+door(ssj, cellar).
+door(ssk, cellar).
+door(ssn, cellar).
+door(sse, cellar).
+door(ssc, cellar).
+door(V, cellar).
+door(/*shdjhkashdkjH*/kevin,cellar).
+/*nishyo hsahsjkhdkjahd*/
+door(kevin,kevinssj).
